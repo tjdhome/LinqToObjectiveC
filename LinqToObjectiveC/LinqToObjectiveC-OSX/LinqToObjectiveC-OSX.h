180daac39b606da0b50cae44855739656052b9e2
t.j. davidson <tjd@thosdavidson.com>
2016-01-13 06:39:00 -0600

    Add targets for TVos and OSX.

---
+++ LinqToObjectiveC/LinqToObjectiveC-OSX/LinqToObjectiveC-OSX.h
@@ -0,0 +1,19 @@
+//
+//  LinqToObjectiveC-OSX.h
+//  LinqToObjectiveC-OSX
+//
+//  Created by Tom Davidson on 1/13/16.
+//  Copyright © 2016 scottlogic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LinqToObjectiveC-OSX.
+FOUNDATION_EXPORT double LinqToObjectiveC_OSXVersionNumber;
+
+//! Project version string for LinqToObjectiveC-OSX.
+FOUNDATION_EXPORT const unsigned char LinqToObjectiveC_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinqToObjectiveC_OSX/PublicHeader.h>
+
+
---
+++ LinqToObjectiveC/LinqToObjectiveC-TVos/LinqToObjectiveC-TVos.h
@@ -0,0 +1,19 @@
+//
+//  LinqToObjectiveC-TVos.h
+//  LinqToObjectiveC-TVos
+//
+//  Created by Tom Davidson on 1/13/16.
+//  Copyright © 2016 scottlogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinqToObjectiveC-TVos.
+FOUNDATION_EXPORT double LinqToObjectiveC_TVosVersionNumber;
+
+//! Project version string for LinqToObjectiveC-TVos.
+FOUNDATION_EXPORT const unsigned char LinqToObjectiveC_TVosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinqToObjectiveC_TVos/PublicHeader.h>
+
+
